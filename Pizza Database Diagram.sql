75c52961d0f8fdd096f0bbb95527ab09a439e7af
Nhi Ton <nhith.vietnam@gmail.com>
2024-11-13 14:00:23 +0700

    finished import database

---
+++ Pizza Database Diagram.sql
@@ -0,0 +1,141 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/pzL8pG
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE `Orders` (
+    `row_id` int  NOT NULL ,
+    `order_id` varchar(10)  NOT NULL ,
+    `customer_id` int  NOT NULL ,
+    `created_date` date  NOT NULL ,
+    `created_at` time  NOT NULL ,
+    `item_id` varchar(10)  NOT NULL ,
+    `quantity` int  NOT NULL ,
+    `delivery` boolean  NOT NULL ,
+    `add_id` int  NOT NULL ,
+    PRIMARY KEY (
+        `row_id`
+    )
+);
+
+CREATE TABLE `Customers` (
+    `customer_id` int  NOT NULL ,
+    `Cus_first_name` varchar(50)  NOT NULL ,
+    `Cus_last_name` varchar(50)  NOT NULL ,
+    PRIMARY KEY (
+        `customer_id`
+    )
+);
+
+CREATE TABLE `Address` (
+    `add_id` int  NOT NULL ,
+    `delivery_address1` varchar(200)  NOT NULL ,
+    `delivery_address2` varchar(200)  NULL ,
+    `delivery_city` varchar(50)  NOT NULL ,
+    `delivery_zipcode` varchar(20)  NOT NULL ,
+    PRIMARY KEY (
+        `add_id`
+    )
+);
+
+CREATE TABLE `Items` (
+    `item_id` varchar(10)  NOT NULL ,
+    `item_sku` varchar(20)  NOT NULL ,
+    `item_name` varchar(100)  NOT NULL ,
+    `item_category` varchar(100)  NOT NULL ,
+    `item_size` varchar(10)  NOT NULL ,
+    `item_price` decimal(10,2)  NOT NULL ,
+    PRIMARY KEY (
+        `item_id`
+    )
+);
+
+CREATE TABLE `Ingredients` (
+    `ing_id` varchar(10)  NOT NULL ,
+    `ing_name` varchar(200)  NOT NULL ,
+    `ing_weight` int  NOT NULL ,
+    `ing_measurements` varchar(20)  NOT NULL ,
+    `ing_price` decimal(5,2)  NOT NULL ,
+    PRIMARY KEY (
+        `ing_id`
+    )
+);
+
+CREATE TABLE `Recipe` (
+    `row_id` int  NOT NULL ,
+    `recipe_id` varchar(20)  NOT NULL ,
+    `ing_id` varchar(10)  NOT NULL ,
+    `quantity` int  NOT NULL ,
+    PRIMARY KEY (
+        `row_id`
+    )
+);
+
+CREATE TABLE `Inventory` (
+    `inv_id` int  NOT NULL ,
+    `item_id` varchar(10)  NOT NULL ,
+    `quantity` int  NOT NULL ,
+    PRIMARY KEY (
+        `inv_id`
+    )
+);
+
+CREATE TABLE `Staff` (
+    `staff_id` varchar(20)  NOT NULL ,
+    `Sta_first_name` varchar(50)  NOT NULL ,
+    `Sta_last_name` varchar(50)  NOT NULL ,
+    `position` varchar(100)  NOT NULL ,
+    `hourly_rate` decimal(5,2)  NOT NULL ,
+    PRIMARY KEY (
+        `staff_id`
+    )
+);
+
+CREATE TABLE `Shift` (
+    `shift_id` varchar(20)  NOT NULL ,
+    `day_of_week` varchar(10)  NOT NULL ,
+    `start_time` time  NOT NULL ,
+    `end_time` time  NOT NULL ,
+    PRIMARY KEY (
+        `shift_id`
+    )
+);
+
+CREATE TABLE `Rotations` (
+    `row_id` int  NOT NULL ,
+    `rota_id` varchar(20)  NOT NULL ,
+    `date` date  NOT NULL ,
+    `shift_id` varchar(20)  NOT NULL ,
+    `staff_id` varchar(20)  NOT NULL ,
+    PRIMARY KEY (
+        `row_id`
+    )
+);
+
+ALTER TABLE `Customers` ADD CONSTRAINT `fk_Customers_customer_id` FOREIGN KEY(`customer_id`)
+REFERENCES `Orders` (`customer_id`);
+
+ALTER TABLE `Address` ADD CONSTRAINT `fk_Address_add_id` FOREIGN KEY(`add_id`)
+REFERENCES `Orders` (`add_id`);
+
+ALTER TABLE `Items` ADD CONSTRAINT `fk_Items_item_id` FOREIGN KEY(`item_id`)
+REFERENCES `Orders` (`item_id`);
+
+ALTER TABLE `Ingredients` ADD CONSTRAINT `fk_Ingredients_ing_id` FOREIGN KEY(`ing_id`)
+REFERENCES `Recipe` (`ing_id`);
+
+ALTER TABLE `Recipe` ADD CONSTRAINT `fk_Recipe_recipe_id` FOREIGN KEY(`recipe_id`)
+REFERENCES `Items` (`item_sku`);
+
+ALTER TABLE `Inventory` ADD CONSTRAINT `fk_Inventory_item_id` FOREIGN KEY(`item_id`)
+REFERENCES `Recipe` (`ing_id`);
+
+ALTER TABLE `Shift` ADD CONSTRAINT `fk_Shift_shift_id` FOREIGN KEY(`shift_id`)
+REFERENCES `Rotations` (`staff_id`);
+
+ALTER TABLE `Rotations` ADD CONSTRAINT `fk_Rotations_date` FOREIGN KEY(`date`)
+REFERENCES `Orders` (`created_date`);
+
+ALTER TABLE `Rotations` ADD CONSTRAINT `fk_Rotations_shift_id` FOREIGN KEY(`shift_id`)
+REFERENCES `Staff` (`staff_id`);
+
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-This is a DA porfollio project about a pizza store named Island Rooster
+This is a DA porfollio project about a pizza store
 
 First, I used the QuickDBD tools to easily design all the tables that I need for the project.
 Then, I created a database on MySQL Workbench and executed the code to create tables
+
+Errors - Import data into tables
+At first, I tried to import data on MySQL Workbench, but it didn't work. Even when I converted a CSV file to a JSON file, I got nothing. Then, I tried to import data on Navicat. It ran well until table Orders and Rotations were finished with errors (I think it's because those files have date and time values).
+So, I copied the data to Google Sheets and downloaded the CSV files. Then, I tried to import data again on MySQL Workbench and it ran successfully.
+
+
+
+I'm going to build:
+
+Dashboard 1 - Order activity, which following data:
+1. Total orders
+2. Total sales
+3. Total items
+4. Average order value
+5. Sales by category
+6. Top selling items
+7. Orders by hour
+8. Sales by hour
+9. Orders by address
+10. Orders by delivery/pick up
+
